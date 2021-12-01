73021e1e1b2b52dfc87030f9874cce5f2c2b5b53
CDeweger <caroljydeweger@gmail.com>
2021-11-30 23:43:49 -0800

    update mobile version header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
+
 import Header from "./components/Header/Header";
+import './styles/_partials/_global.scss';
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,31 @@
-import AvatarImg from "../../assets/images/mohan-muruge.jpg"
+import logo from "../../assets/logo/brainFlix-logo.svg";
+import avatarImg from "../../assets/images/mohan-muruge.jpg";
+import uploadIcon from "../../assets/icons/upload.svg";
+import searchIcon from "../../assets/icons/search.svg";
+import '../../styles/_partials/_global.scss';
+
 
 function Header() {
     return (
         <header className ="brainflix-header">
-            <img className ="brainflix-header__img" src={AvatarImg} alt="mohan-muruge"/>
+            <div className ="brainflix-header__logo-container">
+                <img className ="brainflix-header__logo-container--logo" src={logo} alt="BrainFlix logo"/>
+            </div>
+            <div className ="brainflix-header__search">
+                <img className ="brainflix-header__search--icon" src={searchIcon} alt="search"/>
+                <input className ="brainflix-header__search--input" type="text" placeholder="Search"></input>
+            </div>
+            <div className ="brainflix-header__container">
+                <img className ="brainflix-header__container--img" src={avatarImg} alt="mohan-muruge"/>
+            </div>        
+            <div className ="brainflix-header__button">
+                <img className ="brainflix-header__button--icon" src={uploadIcon} alt="upload"/>
+                <p className ="brainflix-header__button--upload">UPLOAD</p>
+            </div> 
         </header>
+      
     );
 }
 
 export default Header;
+  
