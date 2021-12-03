8d77982aa93f85005a388fa4df8675d98cf8b168
CDeweger <caroljydeweger@gmail.com>
2021-12-02 21:19:06 -0800

    update desktop version header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import Header from "./components/Header/Header";
+import Video from "./components/Video/Video";
+
 import './styles/_partials/_global.scss';
 
 function App() {
   return (
     <div>
      <Header/> 
+     <Video/>
    </div>
   );
 }
