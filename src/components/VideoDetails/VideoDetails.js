7f1db6a2888692e95cff642f78d6874751bebb04
CDeweger <caroljydeweger@gmail.com>
2021-12-04 17:54:18 -0800

    update mobile version  main video section styling

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import './VideoDetails.scss'
-
+import likesImg from "../../assets/icons/likes.svg"
+import viewsImg from "../../assets/icons/views.svg"
 
 const VideoDetails = (props) => {
+
+    const timestampString = (props.selectedVideo.timestamp);
+    const dateObject = new Date(timestampString);
+    const humanReadableDateFormat = dateObject.toLocaleString("en-US", { month: "2-digit" , day: "2-digit", year: "numeric"});
+    
     return (
         <div className="video-main">
             <div className="video-main__video-container">
-                <video className="video-main__video-container--current-video" width="100%vw" height="auto" poster = {props.selectedVideo.image} controls> </video>
+                <video className="video-main__video-container--current-video"  poster = {props.selectedVideo.image} controls> </video>
             </div>
             <h1 className="video-main__heading">{props.selectedVideo.title}</h1>
-            <div className="channel-and-timestamp__container">
-                <p className="channel-and-timestamp__container--channel">By {props.selectedVideo.channel}</p>
-                <p className="channel-and-timestamp__container--timestamp">{props.selectedVideo.timestamp}</p>
-            </div>
-            <div className="views-and-likes__container">
-                <p className="views-and-likes__container--views">{props.selectedVideo.views}</p>
-                <p className="views-and-likes__container--likes">{props.selectedVideo.likes}</p>
+            <div className="video-main__info-container">
+                <div className="video-main__channel-timestamp-container">
+                    <p className="video-main__channel-timestamp-container--channel">By {props.selectedVideo.channel}</p>
+                    <p className="video-main__channel-timestamp-container--timestamp">{humanReadableDateFormat}</p>
+                </div>
+                <div className="video-main__views-likes-container">
+                    <p className="video-main__views-likes-container--views">{props.selectedVideo.views}</p>
+                    <img className="video-main__views-likes-container--views-img" src={viewsImg} alt="views"></img>
+                    <p className="video-main__views-likes-container--likes">{props.selectedVideo.likes}</p>
+                    <img className="video-main__views-likes-container--likes-img"src={likesImg} alt="likes"></img>
+                </div>
             </div>
             <p className="video-main__description">{props.selectedVideo.description}</p>
-       
         </div>
     );
 };
