00debf3f350b0bd0816d12986a42dc97c2ddaf10
CDeweger <caroljydeweger@gmail.com>
2021-12-04 20:25:09 -0800

    update mobile version comments section styling

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './Comments.scss'
+
 
 const Comments = (props) => {
+        // const timestampString = (props.selectedVideoComment.timestamp);
+        // const dateObject = new Date(timestampString);
+        // const humanReadableDateFormat = dateObject.toLocaleString("en-US", { month: "2-digit" , day: "2-digit", year: "numeric"});
     return (
     props.selectedVideoComments.map(selectedVideoComment => 
 
-        <>
-          <p>{selectedVideoComment.name}</p>
-          <p>{selectedVideoComment.comment}</p>
-        </>
+        <div>
+          <div className="comment-box">
+            <div className="comment-box__img"></div>
+            <div className="comment-box__content-box">
+              <div className="comment-box__content-box-name-time">
+                <p className="comment-box__content-box--name">{selectedVideoComment.name}</p>
+                <p className="comment-box__content-box--time">{selectedVideoComment.timestamp}</p>
+              </div>
+              <p className="comment-box__comment">{selectedVideoComment.comment}</p>
+            </div>
+          </div>
+        </div>
     )
   )    
 }
